da3dc32ad807a9a38e637e13c2a098dc920bef02
Luqmaan Sahar <luqmaan.sahar@gmail.com>
2025-10-18 10:00:55 +0100

    added docstrings

--- fed/data.py
+++ fed/data.py
@@ -11,7 +11,21 @@ end_year = 2022
 
 def data_load(units = 1e12, countries=countries, indicator=indicator, 
               start=start_year, end=end_year):
+    """
+    loads GDP data from World Bank
 
+    Parameters
+    ----------
+    units: (float) data units in US$
+    countries: (list) list of country codes to download data for
+    indicator: (str) Dataset to download. Default is annual GDP in current US$
+    start: (int) start year of time series
+    end: (int) end year of time series
+
+    Returns
+    ----------
+    data: (DataFrame) the tidy dataset
+    """
     data = wb.download(indicator=indicator,
                 country=countries,
                 start=start,
--- fed/plot_utils.py
+++ fed/plot_utils.py
@@ -5,6 +5,17 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8-whitegrid')
 
 def plot_line(data):
+    """
+    Creates a simple line plot using maplotlib
+
+    Parameters
+    ----------
+    data: (DataFrame) GDP dataset
+
+    Returns
+    ----------
+    ax: (plt.axes) matplotlib axis object for further customisation.
+    """
     ax = data.plot(figsize=(10,6), kind="line", colormap='tab10')
     ax.set(title="GDP (Trillions USD) 2000–2022", ylabel="GDP (Trillions USD)",
             xlabel="Year")
