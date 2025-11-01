4371bcb1dcf5a0fb33f52bd066df47c21ac3017d
LSahar <150852718+LuqmaanSahar@users.noreply.github.com>
2025-11-01 10:53:25 +0000

    added type hints

--- fed/plot_utils.py
+++ fed/plot_utils.py
@@ -2,19 +2,20 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 plt.style.use('seaborn-v0_8-whitegrid')
 
-def plot_line(data):
+def plot_line(data: pd.DataFrame) -> Axes:
     """
     Creates a simple line plot using maplotlib
 
     Parameters
     ----------
-    data: (DataFrame) GDP dataset
+    data: (pd.DataFrame) GDP dataset
 
     Returns
     ----------
-    ax: (plt.axes) matplotlib axis object for further customisation.
+    ax: (matplotlib.axes.Axes) matplotlib axis object for further customisation.
     """
     ax = data.plot(figsize=(10,6), kind="line", colormap='tab10')
     ax.set(title="GDP (Trillions USD) 2000–2022", ylabel="GDP (Trillions USD)",
